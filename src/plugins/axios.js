7535e3a8e99578946382dea2b5ae69dc5f91e705
hoochicken <mareike.riegel@ql.de>
2022-06-22 12:59:41 +0200

    feat (code) smicolons stripped

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,65 +1,65 @@
-"use strict";
+"use strict"
 
-// import Vue from 'vue';
+// import Vue from 'vue'
 import { createApp } from 'vue'
 const Vue = createApp({})
 
-import axios from "axios";
+import axios from "axios"
 
 // Full config:  https://github.com/axios/axios#request-config
-// axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
-// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+// axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || ''
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN
+// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 
 let config = {
   BASE_URL: import.meta.baseURL || ''
   // BASE_URL_VUE: process.env.baseURL || process.env.apiUrl || ""
   // timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
-};
+}
 
-const _axios = axios.create(config);
+const _axios = axios.create(config)
 
 _axios.interceptors.request.use(
   function(config) {
     // Do something before request is sent
-    return config;
+    return config
   },
   function(error) {
     // Do something with request error
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
 // Add a response interceptor
 _axios.interceptors.response.use(
   function(response) {
     // Do something with response data
-    return response;
+    return response
   },
   function(error) {
     // Do something with response error
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
 Plugin.install = function(Vue, options) {
-  Vue.axios = _axios;
-  window.axios = _axios;
+  Vue.axios = _axios
+  window.axios = _axios
   Object.defineProperties(Vue.prototype, {
     axios: {
       get() {
-        return _axios;
+        return _axios
       }
     },
     $axios: {
       get() {
-        return _axios;
+        return _axios
       }
     },
-  });
-};
+  })
+}
 
 Vue.use(Plugin)
 
-export default Plugin;
+export default Plugin
