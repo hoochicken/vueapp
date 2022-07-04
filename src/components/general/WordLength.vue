3204a40b03b8dae11b1a83c965614a3a1e70d130
hoochicken <mareike.riegel@ql.de>
2022-07-04 14:25:18 +0200

    feat (wordLength) wordlength gui funktioniert

--- src/components/general/WordLength.vue
+++ src/components/general/WordLength.vue
@@ -3,7 +3,7 @@
     <h2>Word Length</h2>
     <div class="d-flex">
       <div v-for="(item, key) in lengthAvailable" v-bind:key="key">
-        <span :class="item === length ? 'float-left btn btn-sm btn-secondary' : 'float-left btn btn-sm'" v-on:click="this.$emit('setWordLength', item)">{{ item }}</span>
+        <span :class="item === length ? 'float-left btn btn-sm btn-secondary' : 'float-left btn btn-sm'" v-on:click="setWordLength(item)">{{ item }}</span>
       </div>
     </div>
   </div>
@@ -16,6 +16,11 @@ export default {
   data: () => {
     return {
       lengthAvailable: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }
+  },
+  methods: {
+    setWordLength(wordLength) {
+      this.$emit('setWordLength', wordLength)
     }
   }
 }
